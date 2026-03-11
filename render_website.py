ab5dae70bc7d4fabab2baed33313a044bae7d9b4
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-03-11 23:52:17 +0300

    Разделил книги на 2 колонки

--- render_website.py
+++ render_website.py
@@ -2,20 +2,18 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import json
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-
 from livereload import Server, shell
-
-
-
+from more_itertools import chunked
 
 
 def on_reload():
     template = env.get_template('template.html')
     with open('meta_data.json', 'r', encoding="utf-8") as file:
         books_data = file.read()
-        books = json.loads(books_data)
+    books = json.loads(books_data)
+    divided_books = list(chunked(books, 2))
 
-    rendered_page = template.render(books=books)
+    rendered_page = template.render(book_rows=divided_books)
 
     with open('index.html', 'w', encoding="utf-8") as file:
         file.write(rendered_page)
