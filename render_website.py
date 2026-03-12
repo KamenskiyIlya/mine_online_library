607b7fb5e78cb4343c51e7aafb21f86d7eb5be41
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-03-12 20:19:05 +0300

    Добавил пагинатор

--- render_website.py
+++ render_website.py
@@ -13,10 +13,14 @@ def on_reload():
     with open('meta_data.json', 'r', encoding="utf-8") as file:
         books_data = file.read()
     books = json.loads(books_data)
-    for page_num, books_by_pages in enumerate(list(chunked(books, 10)), 1):
-        books_by_rows = list(chunked(books_by_pages, 2))
+    books_by_pages = list(chunked(books, 10))
 
-        rendered_page = template.render(book_rows=books_by_rows)
+    for page_num, books_on_page in enumerate(books_by_pages, 1):
+        books_by_rows = list(chunked(books_on_page, 2))
+        rendered_page = template.render(
+            book_rows=books_by_rows,
+            pages_number=len(books_by_pages)
+        )
 
         with open(
             f'pages/index{page_num}.html',
@@ -34,7 +38,7 @@ if __name__ == '__main__':
 
     server = Server()
     server.watch('template.html', on_reload)
-    server.serve(root='.')
+    server.serve(root='pages/index1.html')
 
 # server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 # server.serve_forever()
